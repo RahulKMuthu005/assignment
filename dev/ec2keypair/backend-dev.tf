9a396bbef8107921d9c5b6a0b62fa98595178b5f
RahulKMuthu005 <62294056+RahulKMuthu005@users.noreply.github.com>
2022-07-14 11:14:36 +0530

    Delete dev/ec2keypair directory

--- dev/ec2keypair/backend-dev.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "bidgely-terraform-states-mum"
-    dynamodb_table = "terraform_lock_state"
-    key    = "dev/ec2keypair/ec2keypair.tfstate"
-    region = "ap-south-1"
-    profile = "nonprod"
-  }
-}
--- dev/ec2keypair/dev.tfvars
+++
@@ -1,4 +0,0 @@
-profile = "nonprod"
-region = "ap-south-1"
-key_name = "dev-bidgely-rahulk"
-public_key = ""
