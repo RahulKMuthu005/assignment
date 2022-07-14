04f389e168f4634740c522bfb47617af11679a8f
RahulKMuthu005 <62294056+RahulKMuthu005@users.noreply.github.com>
2022-07-14 10:45:32 +0530

    Delete terraform directory

--- terraform/modules/vpc-demo/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "bidgely-terraform-states-mum"
-    dynamodb_table = "terraform_lock_state"
-    key    = "demo/vpc/vpc.tfstate"
-    region = "ap-south-1"
-    profile = "nonprod"
-  }
-}
